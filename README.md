8248fa059c8631331a929893179e241876e7bfc8
Aiven Timptner (ext.) <aiven.timptner@partner.bmw.de>
2024-05-29 16:43:36 +0200

    Add solution for year 2015 day 1

---
+++ README.md
@@ -0,0 +1,11 @@
+# Advent of Code
+
+My personal solutions for [Advent of Code](https://adventofcode.com/), written in [Rust](https://www.rust-lang.org/).
+
+## Usage
+
+```shell
+cargo run --bin <puzzle>
+```
+
+The `puzzle` name always consists of the year and day: e.g. `1501` (Year 2015, Day 1).
---
+++ src/bin/1501.rs
@@ -0,0 +1,51 @@
+// Year 2015 Day 1
+
+use std::fs::read_to_string;
+
+fn main() {
+    let content = read_puzzle_input();
+    let floor = part1(content.clone());
+    println!("Floor: {}", floor);
+    let instruction = part2(content);
+    println!("Basement: {}", instruction);
+}
+
+fn read_puzzle_input() -> String {
+    let input = read_to_string("./data/2015/01.txt").expect("failed to read file");
+    input
+}
+
+fn part1(content: String) -> i32 {
+    let mut floor: i32 = 0;
+    for direction in content.chars() {
+        floor += match direction as u32 {
+            40 => 1,    // opening parenthesis
+            41 => -1,   // closing parenthesis
+            13 => 0,    // cariage return
+            10 => 0,    // linefeed
+            32 => 0,    // whitespace
+            _ => panic!("unknown character: {}", direction as u32)
+        };
+    }
+    floor
+}
+
+fn part2(content: String) -> i32 {
+    let mut floor: i32 = 0;
+    let mut instruction: i32 = 1;
+    for direction in content.chars() {
+        floor += match direction as u32 {
+            40 => 1,    // opening parenthesis
+            41 => -1,   // closing parenthesis
+            13 => 0,    // cariage return
+            10 => 0,    // linefeed
+            32 => 0,    // whitespace
+            _ => panic!("unknown character: {}", direction as u32)
+        };
+        if floor == -1 {
+            break;
+        }
+        instruction += 1;
+    }
+    instruction
+}
