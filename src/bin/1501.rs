754b92d362977236ccdb3e7a8d79f34c9cc7e3a3
Aiven Timptner (ext.) <aiven.timptner@partner.bmw.de>
2024-09-06 09:54:27 +0200

    add tests

--- src/bin/1501.rs
+++ src/bin/1501.rs
@@ -5,9 +5,11 @@ use std::fs::read_to_string;
 
 fn main() {
     let content = read_puzzle_input();
-    let floor = part1(content.clone());
+    
+    let floor = part1(&content);
     println!("Floor: {}", floor);
-    let instruction = part2(content);
+
+    let instruction = part2(&content);
     println!("Basement: {}", instruction);
 }
 
@@ -16,37 +18,68 @@ fn read_puzzle_input() -> String {
     input
 }
 
-fn part1(content: String) -> i32 {
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part1() {
+        let data = vec![
+            ("(())", 0),
+            ("()()", 0),
+            ("(((", 3),
+            ("(()(()(", 3),
+            ("))(((((", 3),
+            ("())", -1),
+            ("))(", -1),
+            (")))", -3),
+            (")())())", -3),
+        ];
+        for (input, result) in data {
+            assert_eq!(part1(input), result);
+        }
+    }
+
+    #[test]
+    fn test_part2() {
+        let data = vec![
+            (")", 1),
+            ("()())", 5),
+        ];
+        for (input, result) in data {
+            assert_eq!(part2(input), result);
+        }
+    }
+}
+
+fn get_direction(char: char) -> i8 {
+    match char {
+        '(' => 1,
+        ')' => -1,
+        '\r' => 0,
+        '\n' => 0,
+        ' ' => 0,
+        _ => panic!("unknown character: {}", char),
+    }
+}
+
+fn part1(content: &str) -> i32 {
     let mut floor: i32 = 0;
     for direction in content.chars() {
-        floor += match direction as u32 {
-            40 => 1,  // opening parenthesis
-            41 => -1, // closing parenthesis
-            13 => 0,  // cariage return
-            10 => 0,  // linefeed
-            32 => 0,  // whitespace
-            _ => panic!("unknown character: {}", direction as u32),
-        };
+        floor += get_direction(direction) as i32;
     }
     floor
 }
 
-fn part2(content: String) -> i32 {
+fn part2(content: &str) -> i32 {
     let mut floor: i32 = 0;
-    let mut instruction: i32 = 1;
+    let mut instruction: i32 = 0;
     for direction in content.chars() {
-        floor += match direction as u32 {
-            40 => 1,  // opening parenthesis
-            41 => -1, // closing parenthesis
-            13 => 0,  // cariage return
-            10 => 0,  // linefeed
-            32 => 0,  // whitespace
-            _ => panic!("unknown character: {}", direction as u32),
-        };
+        floor += get_direction(direction) as i32;
+        instruction += 1;
         if floor == -1 {
             break;
         }
-        instruction += 1;
     }
     instruction
 }
